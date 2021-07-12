d457c4995475878584dd2776cd5c57f3e663bf18
Amy <74540344+13-8billion@users.noreply.github.com>
2021-07-12 20:09:14 +0100

    authenticaton

--- src/main/java/sustain/project/interfaces/UserRepo.java
+++ src/main/java/sustain/project/interfaces/UserRepo.java
@@ -1,8 +1,12 @@
 package sustain.project.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sustain.project.classes.User;
 
+@org.springframework.stereotype.Repository
 public interface UserRepo extends JpaRepository<User, String> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    User findByEmail(String email);
 
 }
