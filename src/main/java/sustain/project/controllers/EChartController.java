d1115fb3552d056270e61e94d8d44d1453d5d3d2
Amy <74540344+13-8billion@users.noreply.github.com>
2021-07-21 22:09:41 +0100

    houseEnergy charts

--- src/main/java/sustain/project/controllers/EChartController.java
+++ src/main/java/sustain/project/controllers/EChartController.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import sustain.project.models.FoodTotal;
+import sustain.project.models.HouseEnergyTotal;
 import sustain.project.models.OverAllTotal;
 import sustain.project.models.TransportTotal;
-import sustain.project.service.CustomUserDetailsService;
-import sustain.project.service.FoodTotalService;
-import sustain.project.service.OverAllTotalService;
-import sustain.project.service.TransportTotalService;
+import sustain.project.service.*;
 
 
 import java.util.*;
@@ -30,6 +28,8 @@ public class EChartController {
     private OverAllTotalService oats;
     @Autowired
     private TransportTotalService tts;
+    @Autowired
+    private HouseEnergyTotalService hets;
 
     // METHODS
 
@@ -64,6 +64,7 @@ public class EChartController {
         OverAllTotal oat11 = new OverAllTotal();
         OverAllTotal oat12 = new OverAllTotal();
 
+        // FOOD
         for (FoodTotal foodTotal : ft) {
             if (foodTotal.getDate().getMonthValue() == 1 && foodTotal.getUsername().equals(username)) {
                 total1 = total1 + foodTotal.getTotalCo2();
@@ -163,19 +164,17 @@ public class EChartController {
             }
 
             List<Double> total = oats.listAll().stream().map(x -> x.getTotal()).collect(Collectors.toList());
-            List<String> months = oats.listAll().stream().map(x -> x.getDate()).collect(Collectors.toList());
-
-
             model.addAttribute("total", total);
-            model.addAttribute("month", months);
+
 
         }
         Iterable<OverAllTotal> deleteTotalObject = oats.listAll();
         oats.deleteAll(deleteTotalObject);
 
+
+        // TRANSPORT
+
         List<TransportTotal> tt = tts.listAll();
-
-
         for (TransportTotal transTotal : tt) {
 
 
@@ -277,15 +276,124 @@ public class EChartController {
             }
 
             List<Double> totalTrans = oats.listAll().stream().map(x -> x.getTotal()).collect(Collectors.toList());
-            List<String> monthsTrans = oats.listAll().stream().map(x -> x.getDate()).collect(Collectors.toList());
-
-
             model.addAttribute("totalTrans", totalTrans);
-            model.addAttribute("monthTrans", monthsTrans);
 
         }
         deleteTotalObject = oats.listAll();
         oats.deleteAll(deleteTotalObject);
+
+
+        // HOUSE ENERGY
+
+        List<HouseEnergyTotal> het = hets.listAll();
+        for (HouseEnergyTotal houseTotal : het) {
+
+            if (houseTotal.getDate().getMonthValue() == 1 && houseTotal.getUsername().equals(username)) {
+                total1 = houseTotal.getTotal();
+                oat1.setUsername(username);
+                oat1.setTotal(total1);
+                oat1.setDate("Jan");
+                oats.save(oat1);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 2 && houseTotal.getUsername().equals(username)) {
+                total2 = houseTotal.getTotal();
+                oat2.setUsername(username);
+                oat2.setTotal(total2);
+                oat2.setDate("Feb");
+                oats.save(oat2);
+            }
+
+
+            if (houseTotal.getDate().getMonthValue() == 3 && houseTotal.getUsername().equals(username)) {
+                total3 = houseTotal.getTotal();
+                oat3.setUsername(username);
+                oat3.setTotal(total3);
+                oat3.setDate("Mar");
+                oats.save(oat3);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 4 && houseTotal.getUsername().equals(username)) {
+                total4 = houseTotal.getTotal();
+                oat4.setUsername(username);
+                oat4.setTotal(total4);
+                oat4.setDate("Apr");
+                oats.save(oat4);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 5 && houseTotal.getUsername().equals(username)) {
+                total5 = houseTotal.getTotal();
+                oat5.setUsername(username);
+                oat5.setTotal(total5);
+                oat5.setDate("May");
+                oats.save(oat5);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 6 && houseTotal.getUsername().equals(username)) {
+                total6 = houseTotal.getTotal();
+                oat6.setUsername(username);
+                oat6.setTotal(total6);
+                oat6.setDate("Jun");
+                oats.save(oat6);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 7 && houseTotal.getUsername().equals(username)) {
+                total7 = houseTotal.getTotal();
+                oat7.setUsername(username);
+                oat7.setTotal(total7);
+                oat7.setDate("Jul");
+                oats.save(oat7);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 8 && houseTotal.getUsername().equals(username)) {
+                total8 = houseTotal.getTotal();
+                oat8.setUsername(username);
+                oat8.setTotal(total8);
+                oat8.setDate("Aug");
+                oats.save(oat8);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 9 && houseTotal.getUsername().equals(username)) {
+                total9 = houseTotal.getTotal();
+                oat9.setUsername(username);
+                oat9.setTotal(total9);
+                oat9.setDate("Sep");
+                oats.save(oat9);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 10 && houseTotal.getUsername().equals(username)) {
+                total10 = houseTotal.getTotal();
+                oat10.setUsername(username);
+                oat10.setTotal(total10);
+                oat10.setDate("Oct");
+                oats.save(oat10);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 11 && houseTotal.getUsername().equals(username)) {
+                total11 = houseTotal.getTotal();
+                oat11.setUsername(username);
+                oat11.setTotal(total11);
+                oat11.setDate("Nov");
+                oats.save(oat11);
+            }
+
+            if (houseTotal.getDate().getMonthValue() == 12 && houseTotal.getUsername().equals(username)) {
+                total12 = total12 + houseTotal.getTotal();
+                oat12.setUsername(username);
+                oat12.setTotal(total12);
+                oat12.setDate("Dec");
+                oats.save(oat12);
+            }
+
+            List<Double> totalHouse = oats.listAll().stream().map(x -> x.getTotal()).collect(Collectors.toList());
+            model.addAttribute("totalHouse", totalHouse);
+
+
+        }
+        deleteTotalObject = oats.listAll();
+        oats.deleteAll(deleteTotalObject);
+
+
         return "statistics";
 
     }
--- src/main/java/sustain/project/service/CustomUserDetailsService.java
+++ src/main/java/sustain/project/service/CustomUserDetailsService.java
@@ -1,5 +1,4 @@
 package sustain.project.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
